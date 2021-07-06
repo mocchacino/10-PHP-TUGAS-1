110eb46abf10be9f49e679359860be87ed3f63fd
wiladhianty <wiladhianty@gmail.com>
2021-07-06 12:30:31 +0700

    tambah tugas

---
+++ tugas1.php
@@ -0,0 +1,16 @@
+<?php
+    echo "
+        <h1>Daftar Menu</h1>
+        <ul>
+            <li>Ayam Goreng = Rp. 13000</li>
+            <li>Ayam Bakar = Rp.15000</li>
+            <li>Ayam Sayur = Rp. 13000</li>
+            <li>Tempe Goreng = Rp. 1000</li>
+            <li>Tahu Goreng = Rp. 1000</li>
+            <li>Soto = Rp. 3000</li>
+            <li>Nasi Putih = Rp. 5000</li>
+            <li>Es Teh = Rp. 4000</li>
+            <li>Es Tebu = Rp. 50000</li>
+        </ul>
+    ";
+?>
---
+++ tugas2.php
@@ -0,0 +1,52 @@
+<?php
+    $cireng = 'Cireng';
+    $harga_cireng = 1000;
+
+    $es_duren = 'Es Duren';
+    $harga_es_duren = 15000;
+
+    $es_milo = 'Es Milo';
+    $harga_es_milo = 20000;
+
+    $es_teh = 'Es Teh';
+    $harga_es_teh = 5000;
+
+    $piscok = 'Piscok';
+    $harga_piscok = 1000;
+
+    echo "
+        <h1>DAFTAR MENU</h1>
+        <table border=1>
+            <tr>
+                <td>No.</td>
+                <td>Nama Menu</td>
+                <td>Harga</td>
+            </tr>
+            <tr>
+                <td>1.</td>
+                <td>$cireng</td>
+                <td>$harga_cireng</td>
+            </tr>
+            <tr>
+                <td>2.</td>
+                <td>$es_duren</td>
+                <td>$harga_es_duren</td>
+            </tr>
+            <tr>
+                <td>3.</td>
+                <td>$es_milo</td>
+                <td>$harga_es_milo</td>
+            </tr>
+            <tr>
+                <td>4.</td>
+                <td>$es_teh</td>
+                <td>$harga_es_teh</td>
+            </tr>
+            <tr>
+                <td>5.</td>
+                <td>$piscok</td>
+                <td>$harga_piscok</td>
+            </tr>
+        </table>
+    ";
+?>
